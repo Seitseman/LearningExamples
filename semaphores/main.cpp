40b9930351b0f4809d5b4fc58623f8ee42876569
Seitseman <Seitseman@users.noreply.github.com>
2018-04-11 14:57:51 +0300

    Added threads and semaphore

--- semaphores/main.cpp
+++ semaphores/main.cpp
@@ -1,8 +1,52 @@
 #include <QCoreApplication>
+#include <QRandomGenerator>
+#include <QSemaphore>
+#include <QThread>
+
+
+const int DataSize = 100000;
+const int BufferSize = 8192;
+char buffer[BufferSize];
+
+QSemaphore freeBytes(BufferSize);
+QSemaphore usedBytes;
+
+class Producer : public QThread
+{
+public:
+    void run() override {
+        for (int i = 0; i < DataSize; ++i) {
+            freeBytes.acquire();
+            buffer[i % BufferSize] = "ACGT"[QRandomGenerator::global()->bounded(4)];
+            usedBytes.release();
+        }
+    }
+};
+
+class Consumer : public QThread
+{
+public:
+    void run() override {
+        for (int i = 0; i < DataSize; ++i) {
+            usedBytes.acquire();
+            fprintf(stderr, "%c",buffer[i % BufferSize]);
+            freeBytes.release();
+        }
+        fprintf(stderr, "\n");
+    }
+};
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    Producer producer;
+    Consumer consumer;
+
+    producer.start();
+    consumer.start();
+
+    consumer.wait();
 
     return a.exec();
 }
